c1ff4987fd74ad085b3475ae41813b45c8db5a68
impanyu <impanyu@gmail.com>
2025-02-15 15:14:39 -0600

    test

--- chatgpt_django_app/urls.py
+++ chatgpt_django_app/urls.py
@@ -21,8 +21,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', include("chatgpt_django_app.chat.urls")),
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='login.html'), name='logout'),
 ]
 
 
