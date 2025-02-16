a199ac2a6287ef15ffcc4fdbb7550de4603b6d31
impanyu <impanyu@gmail.com>
2025-02-16 13:43:57 -0600

    test

--- chatgpt_django_app/chat/urls.py
+++ chatgpt_django_app/chat/urls.py
@@ -9,7 +9,7 @@ from .views import (
 app_name = "chat"
 
 urlpatterns = [
-    path("chat/", index_view, name="index"),
+    path("", index_view, name="index"),
     path("chat/<int:chat_pk>/message/list/", message_list_view, name="message-list"),
     path("chat/<int:chat_pk>/message/create/", message_create_view, name="message-create"),
 ]
