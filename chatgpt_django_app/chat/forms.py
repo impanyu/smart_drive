55cebb29e132e56b245086742977ef717c667726
impanyu <impanyu@gmail.com>
2025-02-16 12:27:43 -0600

    test

--- chatgpt_django_app/chat/forms.py
+++ chatgpt_django_app/chat/forms.py
@@ -3,6 +3,16 @@ from django.core.validators import MinLengthValidator
 
 from .models import Chat, Message
 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
 
 class MessageForm(forms.ModelForm):
     content = forms.CharField(
